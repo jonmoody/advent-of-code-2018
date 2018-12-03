03cb62703bd9c6604bbe824c96be3c0648c7197b
Jonathan Moody <jonmoody12@gmail.com>
2018-12-02 23:59:52 -0500

    Created shell for day 3

---
+++ src/Day3.h
@@ -0,0 +1,8 @@
+#ifndef DAY3_H
+#define DAY3_H
+
+class Day3 {
+
+};
+
+#endif // DAY3_H
---
+++ src/tests/Day3Test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include "Day3.h"
+
+class Day3Test : public ::testing::Test {
+protected:
+    Day3 subject;
+};
+
