410617034b997843449b70b1222b0ac5566b5453
Jonathan Moody <jonmoody12@gmail.com>
2018-12-04 10:33:04 -0500

    Correct sorting

--- src/tests/Day4Test.cpp
+++ src/tests/Day4Test.cpp
@@ -11,15 +11,17 @@ TEST_F(Day4Test, TheInputIsOrganizedInChronologicalOrder) {
     subject.parseItem("[1518-09-03 00:12] falls asleep");
     subject.parseItem("[1518-04-21 00:04] Guard #3331 begins shift");
     subject.parseItem("[1518-10-29 00:51] falls asleep");
+    subject.parseItem("[1518-04-21 23:55] Guard #3331 begins shift");
     subject.parseItem("[1518-06-12 00:02] Guard #3109 begins shift");
 
     subject.sortItems();
 
     EXPECT_EQ("[1518-04-21 00:04] Guard #3331 begins shift", subject.orderedItems.at(0));
     EXPECT_EQ("[1518-04-21 00:57] wakes up", subject.orderedItems.at(1));
-    EXPECT_EQ("[1518-06-12 00:02] Guard #3109 begins shift", subject.orderedItems.at(2));
-    EXPECT_EQ("[1518-09-03 00:12] falls asleep", subject.orderedItems.at(3));
-    EXPECT_EQ("[1518-10-29 00:51] falls asleep", subject.orderedItems.at(4));
+    EXPECT_EQ("[1518-04-21 23:55] Guard #3331 begins shift", subject.orderedItems.at(2));
+    EXPECT_EQ("[1518-06-12 00:02] Guard #3109 begins shift", subject.orderedItems.at(3));
+    EXPECT_EQ("[1518-09-03 00:12] falls asleep", subject.orderedItems.at(4));
+    EXPECT_EQ("[1518-10-29 00:51] falls asleep", subject.orderedItems.at(5));
 }
 
 TEST_F(Day4Test, TheNumberOfMinutesTheGuardsHaveBeenAsleepAreRecorded) {
